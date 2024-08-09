9b30a17e2ffbaa5fa0f0a74d13aab4a46cff2f8e
Nimesh Vasani <64627988+NimeshVasani@users.noreply.github.com>
2024-08-09 16:47:39 -0400

    UI and bug

--- composeApp/src/commonMain/kotlin/org/nmvasani/tictactoe/ui/screens/SelectionScreen.kt
+++ composeApp/src/commonMain/kotlin/org/nmvasani/tictactoe/ui/screens/SelectionScreen.kt
@@ -1,9 +1,7 @@
 package org.nmvasani.tictactoe.ui.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.Indication
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.indication
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,22 +12,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.LocalMinimumInteractiveComponentEnforcement
-import androidx.compose.material.RadioButton
-import androidx.compose.material.RadioButtonDefaults
 import androidx.compose.material.Text
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight.Companion.W600
 import androidx.compose.ui.unit.dp
@@ -41,7 +32,6 @@ import tictactoe.composeapp.generated.resources.Res
 import tictactoe.composeapp.generated.resources.cross
 import tictactoe.composeapp.generated.resources.zero
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun SelectionScreen(modifier: Modifier = Modifier) {
     var selected by remember { mutableStateOf(1) }
